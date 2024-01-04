a693b40d3701155b202f25ac65f51b1ba7a7b89e
Coupile <1172464137@qq.com>
2024-01-04 08:54:50 +0800

    support ipv6 group

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 目前只搞了ipv4
-
 import requests
 import hashlib
 import sys
@@ -27,15 +25,15 @@ def main():
     # 登录到 iKuai 主机
     login_to_ikuai(session, iKuaiHostIP, username, md5passwd)
 
-    # 获取已有的IP组
-    ip_groups = get_ip_groups(session, iKuaiHostIP)
-
     # 删除包含指定组名的现有IP组
-    delete_ip_groups(session, iKuaiHostIP, ip_groups, groupName)
+    ip_groups = get_ip_groups(session, iKuaiHostIP, "IPv4")
+    delete_ip_groups(session, iKuaiHostIP, ip_groups, groupName, "IPv4")
+    ipv6_groups = get_ip_groups(session, iKuaiHostIP, "IPv6")
+    delete_ip_groups(session, iKuaiHostIP, ipv6_groups, groupName, "IPv6")
 
     # 每1000个IP确认一次后上传IP组
     upload_and_confirm(session, iKuaiHostIP, int(groupID), groupName, ipv4_lines, "IPv4")
-    # upload_and_confirm(session, iKuaiHostIP, int(groupID), groupName, ipv6_lines, "IPv6")
+    upload_and_confirm(session, iKuaiHostIP, int(groupID), groupName, ipv6_lines, "IPv6")
 
 def parse_arguments():
     # 解析命令行参数
@@ -69,25 +67,35 @@ def login_to_ikuai(session, iKuaiHostIP, username, md5passwd):
         "passwd": md5passwd
     })
 
-def del_ipgroup_function(session, iKuaiHostIP, groupID):
+def del_ipgroup_function(session, iKuaiHostIP, groupID, ip_type):
     # 调用 'ipgroup' 函数删除IP组
-    session.post("http://{}/Action/call".format(iKuaiHostIP), json={"func_name": "ipgroup", "action": "del",
-                                                                      "param": {"id": groupID}})
+    json_data ={"action": "del","param": {"id": groupID}}
+    if ip_type == "IPv4":
+        json_data["func_name"] = "ipgroup"
+    if ip_type == "IPv6":
+        json_data["func_name"] = "ipv6group"
+    session.post("http://{}/Action/call".format(iKuaiHostIP), json=json_data)
 
-def upload_ip_group(session, iKuaiHostIP, ipgroup, ip_type):
+def upload_ip_group(session, iKuaiHostIP, ipgroup):
     # 上传IP组信息
     files = {'ipgroup.txt': ipgroup.encode()}
     session.post("http://{}/Action/upload".format(iKuaiHostIP), files=files)
 
-def call_ipgroup_function(session, iKuaiHostIP, ip_type=None):
+def call_ipgroup_function(session, iKuaiHostIP, ip_type):
     # 调用 'ipgroup' 函数导入IP组
-    json_data = {"func_name": "ipgroup", "action": "IMPORT", "param": {"filename": "ipgroup.txt", "append": 1}}
-    if ip_type:
-        json_data["param"]["ip_type"] = ip_type
+    json_data = {"action": "IMPORT", "param": {"filename": "ipgroup.txt", "append": 1}}
+    if ip_type == "IPv4":
+        json_data["func_name"] = "ipgroup"
+    if ip_type == "IPv6":
+        json_data["func_name"] = "ipv6group"
 
     session.post("http://{}/Action/call".format(iKuaiHostIP), json=json_data)
 
 def upload_and_confirm(session, iKuaiHostIP, start_group_id, start_group_name, ip_list, ip_type):
+    if ip_type == "IPv4":
+        start_group_name = "CN_ip_list"
+    if ip_type == "IPv6":
+        start_group_name = "CN_ipv6_list"
     # 确定所需的组数
     num_groups = (len(ip_list) + 999) // 1000
 
@@ -100,21 +108,24 @@ def upload_and_confirm(session, iKuaiHostIP, start_group_id, start_group_name, i
         current_group_id = start_group_id + group_num
         current_group_name = "{}_{}".format(start_group_name, group_num + 1)
         ipgroup = "id={} comment=, group_name={} addr_pool={}".format(current_group_id, current_group_name, ','.join(current_ip_list))
-        del_ipgroup_function(session, iKuaiHostIP, current_group_id)
 
         # 上传IP组信息
-        upload_ip_group(session, iKuaiHostIP, ipgroup, ip_type)
+        upload_ip_group(session, iKuaiHostIP, ipgroup)
 
         # 根据IP类型调用 'ipgroup' 函数导入IP组
         call_ipgroup_function(session, iKuaiHostIP, ip_type)
 
-def get_ip_groups(session, iKuaiHostIP):
+def get_ip_groups(session, iKuaiHostIP, ip_type):
     # 获取IP组列表
-    response = session.post(f"http://{iKuaiHostIP}/Action/call", json={
-        "func_name": "ipgroup",
+    json_data = {
         "action": "show",
         "param": {"TYPE": "total,data", "limit": "0,100", "ORDER_BY": "", "ORDER": ""}
-    }).json()
+    }
+    if ip_type == "IPv4":
+        json_data["func_name"] = "ipgroup"
+    if ip_type == "IPv6":
+        json_data["func_name"] = "ipv6group"
+    response = session.post(f"http://{iKuaiHostIP}/Action/call", json=json_data).json()
 
     if response["Result"] == 30000:
         return response.get("Data", {}).get("data", [])
@@ -122,13 +133,17 @@ def get_ip_groups(session, iKuaiHostIP):
         print(f"获取IP组时出错: {response['ErrMsg']}")
         return []
 
-def delete_ip_groups(session, iKuaiHostIP, ip_groups, group_name):
+def delete_ip_groups(session, iKuaiHostIP, ip_groups, group_name, ip_type):
     # 删除指定组名的IP组
+    if ip_type == "IPv4":
+        group_name = "CN_ip_list"
+    if ip_type == "IPv6":
+        group_name = "CN_ipv6_list"
     for ip_group in ip_groups:
         gname = ip_group.get("group_name", "")
         if group_name in gname:
             group_id = ip_group.get("id", "")
-            del_ipgroup_function(session, iKuaiHostIP, group_id)
+            del_ipgroup_function(session, iKuaiHostIP, group_id, ip_type)
             print(f"已删除ID为 {ip_group['id']}，名称为 {ip_group['group_name']} 的IP组。")
 
 if __name__ == "__main__":
